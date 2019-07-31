f9d6d3614b0de59933cb8c29d91f8e9c3928d667
AnhQuocIT <32187187+AnhQuocIT@users.noreply.github.com>
2019-07-31 16:51:02 +0700

    update route for reset password

--- lib/routes/web.php
+++ lib/routes/web.php
@@ -126,5 +126,54 @@ Route::group(['namespace'=>'Admin'],function(){
 	});
 
 });
-Auth::routes();
+//Clear Cache facade value:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
 
+//Reoptimized class loader:
+Route::get('/optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Clear Config cleared</h1>';
+});
+
+// Auth::routes();
+// Authentication Routes...
+// $this->get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
+// $this->post('admin/login', 'Auth\LoginController@login');
+// $this->post('admin/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+// $this->get('admin/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// $this->post('admin/register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('admin/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('admin/password/reset', 'Auth\ResetPasswordController@reset');
